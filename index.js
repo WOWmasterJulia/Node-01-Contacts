a06a48d57454cca0d568f1b45e77363355fdcf91
WOWmasterJulia <topazzz@ukr.net>
2023-08-06 13:52:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const {program} = require("commander");
-
 const contacts = require("./contacts.js");
+
+program
+    // .option("-a, --action, <type>")
+    // .option("-i, --id, <type>")
+    // .option("-t, --name, <type>")
+    // .option("-at, --emeil, <type>")
+    // .option("-at, --phone, <type>");
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse();
+const options = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
       console.log(action);
@@ -14,27 +28,16 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
         case "add":
             const newContact = await contacts.addContact({name, email, phone});
             return console.log(newContact);
-        
+        case "remove":
+            const deleteContact = await contacts.removeContact(id);
+            return console.log(deleteContact);
+        default: 
+            return console.log("Unknown action")
         
         // case "updateById":
         //     const updateContact = await contacts.updateById(id, {name, email, phone});
         //     return console.log(updateContact);
-        case "deleteById":
-            const deleteContact = await contacts.deleteById(id);
-            return console.log(deleteContact);
-        default: 
-            return console.log("Unknown action")
     }
 }
 
-program
-    .option("-a, --action, <type>")
-    .option("-i, --id, <type>")
-    .option("-t, --name, <type>")
-    .option("-at, --emeil, <type>")
-    .option("-at, --phone, <type>");
-
-program.parse();
-
-const options = program.opts();
 invokeAction(options)
