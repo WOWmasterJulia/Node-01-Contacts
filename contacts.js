62b7d93b3d5db1144cdc434c0353dcbca6a53343
WOWmasterJulia <topazzz@ukr.net>
2023-08-06 15:10:26 +0300

    Changes

--- contacts.js
+++ contacts.js
@@ -8,60 +8,75 @@ const { nanoid } = require("nanoid");
 //     .then(data => console.log(data))
 //     .catch(error => console.table(error.message));
 
-
 /*
  * Раскомментируй и запиши значение
 const contactsPath = ;
  */
-const contactsPath = path.join(__dirname, 'db', 'contacts.json');
-
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 // // TODO: задокументировать каждую функцию
-
 
 // function listContacts() {
 //     // ...твой код. Возвращает массив контактов.
 // }
 const listContacts = async () => {
+  try {
     const data = await fs.readFile(contactsPath);
     return JSON.parse(data);
-}
-  
+  } catch (error) {
+    console.log("Error:", error.message);
+  }
+};
+
 // function getContactById(contactId) {
 //     // ...твой код. Возвращает объект контакта с таким id. Возвращает null, если объект с таким id не найден.
 // }
- const getContactById = async(id) => {
+const getContactById = async (id) => {
+  try {
     const contacts = await listContacts();
-    const result = contacts.find(elem => elem.id === id);
-    return result || null; 
+    const result = contacts.find((elem) => elem.id === id);
+    return result || null;
+  } catch (error) {
+    console.log("Error:", error.message);
   }
+};
 
 // function removeContact(contactId) {
 //     // ...твой код. Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
 // }
-const removeContact = async(id) => {
+const removeContact = async (id) => {
+  try {
     const contacts = await listContacts();
-    const index = contacts.findIndex(elem => elem.id === id);
-    if (index === -1) {return null};
-    const [result] = contacts.splice(index,1);
+    const index = contacts.findIndex((elem) => elem.id === id);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
     await fs.writeFile(contactsPath, JSON.stringify(contacts));
     return result;
-}
-  
+  } catch (error) {
+    console.log("Error:", error.message);
+  }
+};
+
 // function addContact(name, email, phone) {
 //     // ...твой код. Возвращает объект добавленного контакта.
 // }
- const addContact = async(data) => {
+const addContact = async (data) => {
+  try {
     const contacts = await listContacts();
     const newContact = { id: nanoid(), ...data };
     contacts.push(newContact);
     await fs.writeFile(contactsPath, JSON.stringify(contacts));
     return newContact;
+  } catch (error) {
+    console.log("Error:", error.message);
   }
+};
 
-  module.exports = {
-    listContacts,
-    getContactById,
-    addContact,
-    removeContact
-}
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
